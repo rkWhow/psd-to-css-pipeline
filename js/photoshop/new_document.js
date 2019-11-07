8f290202699076e68111076c064264f016328a0b
Radoslaw Kamysz <r.kamysz@whow.net>
2019-11-07 15:48:40 +0100

    mend

--- js/photoshop/new_document.js
+++ js/photoshop/new_document.js
@@ -1,4 +1,4 @@
-(function(){
-  var doc = app.documents.add(1000,1000, 72, "Test DDDDD", NewDocumentMode.RGB, DocumentFill.TRANSPARENT, 1);
+(function(width, height, title){
+  app.documents.add(width, height, 72, title, NewDocumentMode.RGB, DocumentFill.TRANSPARENT, 1);
   return true;
 }());
